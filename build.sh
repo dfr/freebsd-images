53f4d0a5754ce74e2accbcf98e15b60ec1d089ef
Doug Rabson <dfr@rabson.org>
2023-11-10 08:11:25 +0000

    Remove the pf image and add more timestamps for 13.2
    Now that it is possible to install extra pkgbase packages from
    pkg.freebsd.org, there is no need for specialised images such as pf.

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ build() {
     local branch=$(echo $spec | cut -d: -f1)
     local ver=$(echo $spec | cut -d: -f2)
 
-    for image in mtree static base minimal small pf; do
+    for image in mtree static base minimal small; do
 	./build-${image}.sh -A "${ARCHES}" -b ${branch} ${ver} || exit 1
     done
 }
@@ -20,7 +20,7 @@ push() {
     local branch=$(echo $spec | cut -d: -f1)
     local ver=$(echo $spec | cut -d: -f2)
 
-    for image in mtree static base minimal small pf; do
+    for image in mtree static base minimal small; do
 	./build-${image}.sh -A "${ARCHES}" -p ${branch} ${ver} || exit 1
     done
 }
@@ -45,11 +45,14 @@ while getopts "A:P:bp" arg; do
 done
 shift $((OPTIND-1))
 
-builds="\
-	main:15 \
-	stable/14:14 \
-	stable/13:13 \
-	releng/13.2:13.2"
+# Packages for these branches are available on pkg.freebsd.org
+builds="main:15 stable/14:14 releng/14.0:14.0"
+
+# FreeBSD-13 builds are supported on my home lab infrastructure - hopefully
+# these will also be added to pkg.freebsd.org soon.
+if [ "${REPO_INSTALL_URL}" = "http://pkgbase.home.rabson.org" ]; then
+   builds="${builds} stable/13:13 releng/13.2:13.2"
+fi
 
 if [ $# -gt 0 ]; then
     builds="$@"
